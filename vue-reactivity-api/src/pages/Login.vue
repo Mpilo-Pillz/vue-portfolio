2dc294ca9e3d8e09ce9ae4107c0a4575f9ad7315
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-04-28 12:12:45 +0200

    sehlving in favor of veutify

---
+++ vue-reactivity-api/src/pages/Login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Login from '../components/auth/Login.vue'
+</script>
+
+<template>
+  <h1>Login Page</h1>
+<Login />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
