34a324d4939ed6531e76e327f68fae8dd3167d10
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-04-30 13:13:45 +0200

    initial project scaffold

---
+++ vuetify-pinia/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <Header />
+      <HelloWorld />
+
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import Header from '@/components/shared/Header.vue'
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
