01e07ecc7761711f28b52f7e2a395fb7aded7a3f
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2022-05-08 14:13:45 +0200

    moving drawer to app component

--- vuetify-pinia/src/App.vue
+++ vuetify-pinia/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app>
     <v-main>
-      <Header />
+      <Header @onClick="toggle" />
+      <NavigationDrawer  :isOpen="isOpen"/>
       <HelloWorld />
 
     </v-main>
@@ -10,5 +11,10 @@
 
 <script setup lang="ts">
   import Header from '@/components/shared/Header.vue'
+  import NavigationDrawer from '@/components/shared/NavigationDrawer.vue'
   import HelloWorld from '@/components/HelloWorld.vue'
+  import { ref } from 'vue'
+
+  let isOpen = ref<boolean>(false)
+  const toggle = () => isOpen.value = !isOpen.value
 </script>
--- vuetify-pinia/src/components/shared/Header.vue
+++ vuetify-pinia/src/components/shared/Header.vue
@@ -1,3 +1,19 @@
 <template>
-    <v-app-bar></v-app-bar>
+    <v-app-bar
+        >
+          <template v-slot:prepend>
+            <v-app-bar-nav-icon
+            @click.stop="$emit('onClick')"
+            ></v-app-bar-nav-icon>
+          </template>
+  
+          <v-app-bar-title>Home Uitility Management</v-app-bar-title>
+  
+          <template v-slot:append>
+            <v-btn icon="mdi-dots-vertical"></v-btn>
+          </template>
+        </v-app-bar>
 </template>
+<script setup lang="ts">
+
+</script>
